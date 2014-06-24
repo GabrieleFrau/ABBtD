1ea2b0f016142afc2ad7660c205ec45672ec4d4b
GabrieleFrau <streloklele@gmail.com>
2014-06-24 03:10:58 +0200

    Useless as of now

---
+++ include/Collision.hpp
@@ -0,0 +1,11 @@
+#ifndef COLLISION_HPP
+#define COLLISION_HPP
+#include <SFML/Graphics/Shape.hpp>
+namespace My
+{
+    namespace CollisionHandler
+    {
+        //bool AreColliding(sf::Rect<int>& obj1, sf::Rect<int>& obj2);
+    }
+}
+#endif
---
+++ src/Collision.cpp
@@ -0,0 +1,11 @@
+#include <Collision.hpp>
+namespace My
+{
+    namespace CollisionHandler
+    {
+        //bool AreColliding(sf::Rect<int>& obj1, sf::Rect<int>& obj2)
+        //{
+        //    return obj1.instersects(obj2);
+        //}
+    }
+}
